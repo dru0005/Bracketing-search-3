945018bd7279ef317148cf09b6196e9bf70b3308
RileyLaptop <58671459+dru0005@users.noreply.github.com>
2019-12-09 13:56:45 +1100

    top-min num

--- Bracketing Search 3/Bracketing Search 3.cpp
+++ Bracketing Search 3/Bracketing Search 3.cpp
@@ -13,6 +13,8 @@ int main() {
 	int l = 0;
 	int arrayNum = 100;
 	int arrayNumH = 0;
+	int topNum = 0;
+	int lowNum = 0;
 
 	int numbers[101];
 	for (int i = 0; i <= 100; i++)
@@ -20,7 +22,7 @@ int main() {
 
 	srand(time(0));
 
-	cGuess = rand() % 100 + 1;
+	cGuess = 50;
 
 	cout << "Please come up with a number \n" << "Make sure to remember it \n" << "Computer will then guess the number \n";
 
@@ -30,21 +32,39 @@ int main() {
 		cout << cGuess << "\n" << "If higher press 1 \n" << "If lower press 0 \n" << "If correct press 3 \n";
 		cin >> hL;
 
-		h = cGuess / 2;
-		l = cGuess / 2;
+	
 
 		switch (hL) {
 		case 0:
-			arrayNum = arrayNum / 2;
-			cGuess = numbers[arrayNum];
+			l = cGuess / 2;
+			topNum = cGuess;
+
+			for (int i = 0; i < cGuess, i > lowNum; i++)
+				numbers[i] = i;
+
+			cGuess = numbers[l];
+
+
 			break;
 
 		case 1:
-			arrayNumH = arrayNum / 2;
-			arrayNum = arrayNum + arrayNumH;
-			cGuess = numbers[arrayNum];
+			h = cGuess / 2;
+			lowNum = cGuess;
+
+			h = h + cGuess;
+			
+			for (int i = cGuess; i < topNum; i++)
+				numbers[i] = i;
+
+			
+
+			cGuess = numbers[h];
+
+			
 			break;
+
 		}
+		
 
 
 		x++;
